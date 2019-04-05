4284a27c0f1518be6828d6e26dc3954081c5034e
SLRAM <stephier2290@gmail.com>
2019-04-05 15:56:20 -0400

    initial

---
+++ FlockApp/FlockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlockApp
+//
+//  Created by Stephanie Ramirez on 4/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
