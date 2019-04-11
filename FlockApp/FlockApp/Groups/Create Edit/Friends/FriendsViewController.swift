db479f4050bbc8eb73b36a1aebd661b80bcaeaa5
SLRAM <stephier2290@gmail.com>
2019-04-11 11:45:50 -0400

    friend section working

--- FlockApp/FlockApp/Groups/Create Edit/Friends/FriendsViewController.swift
+++ FlockApp/FlockApp/Groups/Create Edit/Friends/FriendsViewController.swift
@@ -16,16 +16,27 @@ class FriendsViewController: UIViewController {
 
     weak var delegate: FriendsViewControllerDelegate?
     private let friendsView = FriendsView()
+    var isSearching = false
+
+    var savedFriends = [String]()
+
     
-    let fakeFriends = ["test 1", "test 2"]
+    let friends = ["test 1", "test 2"]
+    
+    var filteredFriends = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 //        friendsView.delegate = self
         view.addSubview(friendsView)
+        
         friendListButton()
+        friendsView.friendSearch.delegate = self
+
         friendsView.myTableView.delegate = self
         friendsView.myTableView.dataSource = self
+        friendsView.myTableView.tableFooterView = UIView()
+
 
     }
     
@@ -53,7 +64,7 @@ class FriendsViewController: UIViewController {
 //            present(alertController, animated: true)
 //        }
         //convert to a list of check off users userIDs
-        delegate?.selectedFriends(friends: fakeFriends)
+        delegate?.selectedFriends(friends: savedFriends)
         self.navigationController?.popViewController(animated: true)
     }
     
@@ -61,15 +72,78 @@ class FriendsViewController: UIViewController {
 }
 extension FriendsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fakeFriends.count
+        if isSearching {
+            return filteredFriends.count
+        } else {
+            return friends.count
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
-        let friend = fakeFriends[indexPath.row]
-        cell.textLabel?.text = friend.description
+//        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+//        let friend = friends[indexPath.row]
+//        cell.textLabel?.text = friend.description
+//        return cell
+        
+        var friend = String()
+        if isSearching {
+            friend = filteredFriends[indexPath.row]
+        } else {
+            friend = friends[indexPath.row]
+        }
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = friend
+        cell.backgroundColor = .clear
+
+        if savedFriends.contains(friend) {
+            cell.accessoryType = .checkmark
+        }
+        
         return cell
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        var friend = String()
+        if isSearching {
+            friend = filteredFriends[indexPath.row]
+            
+        } else {
+            friend = friends[indexPath.row]
+        }
+        //        print(bus)
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            var counter = 0
+            for savedFriend in savedFriends {
+                if savedFriend != friend {
+                    counter += 1
+                } else {
+                    //                    print(counter)
+                    savedFriends.remove(at: counter)
+                }
+            }
+            
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            savedFriends.append(friend)
+        }
+        
     }
     
     
+}
+
+extension FriendsViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            isSearching = false
+            view.endEditing(true)
+        } else {
+            isSearching = true
+            filteredFriends = friends.filter({$0.lowercased().contains(searchText.lowercased())})
+        }
+        friendsView.myTableView.reloadData()
+    }
 }
