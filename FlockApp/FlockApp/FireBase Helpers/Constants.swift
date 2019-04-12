b8314d4e0b72c54b1b6144e10d5b19a8d385a0f1
BironSu <bironsu@pursuit.org>
2019-04-12 16:15:21 -0400

    Profile Upated - can edit and save everything

---
+++ FlockApp/FlockApp/FireBase Helpers/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  FlockApp
+//
+//  Created by Biron Su on 4/12/19.
+//
+
+import UIKit
+
+struct Constants {
+    static let ProfileImagePath = "profileImages/"
+    static let EventImagePath = "eventImages/"
+}
