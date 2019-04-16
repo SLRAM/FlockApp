af4e853cdb8f79ca31c181395f17a8cdd478a777
natmess <nnmessier@gmail.com>
2019-04-16 12:27:07 -0400

    finished cleaning up appdelegate and setting up uitabbarcontroller with custom buttons + color

---
+++ FlockApp/FlockApp/Groups/TabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  TabBarController.swift
+//  FlockApp
+//
+//  Created by Nathalie  on 4/16/19.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let firstViewController = HomeViewController()
+        
+        firstViewController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(named: "event"), tag: 0)
+        
+        let secondViewController = ProfileViewController()
+        
+        secondViewController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(named: "profile"), tag: 1)
+        
+        let thirdViewController = FriendsViewController()
+        
+        thirdViewController.tabBarItem = UITabBarItem(title: "Friends", image: UIImage(named: "friends"), tag: 2)
+        
+        let tabBarList = [firstViewController, secondViewController, thirdViewController]
+        
+        viewControllers = tabBarList
+    }
+    
+
+}
