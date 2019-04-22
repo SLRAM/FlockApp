825182ae0c22c5fef7653f9ad94eb77cafaac7c8
BironSu <bironsu@pursuit.org>
2019-04-22 14:56:04 -0400

    Friend list is fixed hopefully

--- FlockApp/FlockApp/Groups/Friends/FriendsViewController.swift
+++ FlockApp/FlockApp/Groups/Friends/FriendsViewController.swift
@@ -45,8 +45,8 @@ class FriendsViewController: BaseViewController {
         setupTable(keyword: "")
     }
     private func setupTable(keyword: String) {
-            fetchFriends(keyword: keyword)
-        timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(fetchStrangers(keyword:)), userInfo: keyword, repeats: false)
+        fetchFriends(keyword: keyword)
+        //timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(fetchStrangers(keyword:)), userInfo: keyword, repeats: false)
     }
     
     private func fetchFriends(keyword: String) {
@@ -64,17 +64,17 @@ class FriendsViewController: BaseViewController {
                     print("failed to fetch friends with error: \(error.localizedDescription)")
                 } else if let snapshot = snapshot {
                     let test : [String] = snapshot.documents.map {
-                            let dictionary =  $0.data() as? [String:String]
-                            guard let key = dictionary?.keys.first else { return "" }
-                            return key
-                        }
+                        let dictionary =  $0.data() as? [String:String]
+                        guard let key = dictionary?.keys.first else { return "" }
+                        return key
+                    }
                     self!.fetchFriendInfo(list: test, keyword: keyword)
                 }
         }
     }
-    @objc private func fetchStrangers(keyword: Timer) {
-        let text = keyword.userInfo as! String
-        guard let _ = authservice.getCurrentUser() else {
+    @objc private func fetchStrangers(keyword: String) {
+        let text = keyword
+        guard let user = authservice.getCurrentUser() else {
             print("Please log in")
             return
         }
@@ -87,38 +87,47 @@ class FriendsViewController: BaseViewController {
                     guard let friends = self?.friends else { return }
                     if text == "" {
                         self?.strangers.removeAll()
-
                         let str = snapshot.documents.map { UserModel(dict: $0.data()) }
                         str.forEach { userModel in
-                            if !friends.contains(userModel) {
+                            if !friends.contains(userModel) && userModel.userId != user.uid {
                                 self?.strangers.append(userModel)
                             }
                             self!.strangers = (self?.strangers.sorted { $0.displayName.lowercased() < $1.displayName.lowercased() })!
                         }
-                    } else {
-                        self?.strangers.removeAll()
-                        let str = snapshot.documents.map { UserModel(dict: $0.data()) }
+                } else {
+                    self?.strangers.removeAll()
+                    let str = snapshot.documents.map { UserModel(dict: $0.data()) }
+                        .filter({$0.displayName.lowercased().contains(text.lowercased())})
+                        .sorted { $0.displayName.lowercased() < $1.displayName.lowercased() }
+                    str.forEach { userModel in
+                        if !friends.contains(userModel) && userModel.userId != user.uid {
+                            self?.strangers.append(userModel)
+                        }
+                        self!.strangers = (self?.strangers.sorted { $0.displayName.lowercased() < $1.displayName.lowercased() })!
                             .filter({$0.displayName.lowercased().contains(text.lowercased())})
-                            .sorted { $0.displayName.lowercased() < $1.displayName.lowercased() }
-                        str.forEach { userModel in
-                            if !friends.contains(userModel) {
-                                self?.strangers.append(userModel)
-                            }
-                            self!.strangers = (self?.strangers.sorted { $0.displayName.lowercased() < $1.displayName.lowercased() })!
-                                .filter({$0.displayName.lowercased().contains(text.lowercased())})
-                        }
                     }
-                } 
+                }
+            }
         }
     }
     private func fetchFriendInfo(list: [String], keyword: String) {
-        for friend in list {
-            DBService.fetchUser(userId: friend) { (error, user) in
+        guard !list.isEmpty else {
+            fetchStrangers(keyword: keyword)
+            friends = friends.sorted {$0.displayName.lowercased() < $1.displayName.lowercased()}
+            return
+        }
+        var currentList = list
+        guard let friend = currentList.popLast() else {return}
+        DBService.fetchUser(userId: friend) { (error, user) in
                 if let error = error {
                     print("failed to fetch friends with error: \(error.localizedDescription)")
                 } else if let user = user {
-                    self.friends.append(user)
-                }
+                    if keyword == "" {
+                        self.friends.append(user)
+                    } else if user.displayName.lowercased().contains(keyword.lowercased()) {
+                        self.friends.append(user)
+                    }
+                    self.fetchFriendInfo(list: currentList, keyword: keyword)
             }
         }
     }
@@ -128,7 +137,6 @@ extension FriendsViewController: UITableViewDelegate, UITableViewDataSource, UIS
     func numberOfSections(in tableView: UITableView) -> Int {
         return 2
     }
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0 :
@@ -138,17 +146,16 @@ extension FriendsViewController: UITableViewDelegate, UITableViewDataSource, UIS
         default:
             return 0
         }
-        
     }
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+        guard !friends.isEmpty else { return UITableViewCell() }
+        guard !strangers.isEmpty else { return UITableViewCell() }
         switch indexPath.section {
         case 0:
             let userCell = friends[indexPath.row]
             let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
             cell.textLabel?.text = userCell.displayName
-            cell.detailTextLabel?.text = "Friend added"
+            cell.detailTextLabel?.text = "Friend"
             cell.backgroundColor = .clear
             return cell
         case 1 :
@@ -160,13 +167,9 @@ extension FriendsViewController: UITableViewDelegate, UITableViewDataSource, UIS
         default:
             return UITableViewCell()
         }
-        
-        
     }
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
             setupTable(keyword: searchText)
-        friends = friends.filter {$0.displayName.contains(searchText)}
-        
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         guard let indexPath = friendsView.myTableView.indexPathForSelectedRow else {
