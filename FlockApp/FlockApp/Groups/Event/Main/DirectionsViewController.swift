537d0aa085c6abe7584b77535c73334905d2a53d
natmess <nnmessier@gmail.com>
2019-04-29 11:28:34 -0400

    made some small UI updates and improved the maps' get directions feature for events. clicking the button no longer prompts the user to enter their location, it just shows the route to the event immediately.

--- FlockApp/FlockApp/Groups/Event/Main/DirectionsViewController.swift
+++ FlockApp/FlockApp/Groups/Event/Main/DirectionsViewController.swift
@@ -11,13 +11,13 @@ import Firebase
 import FirebaseFirestore
 import CoreLocation
 
-class DirectionsViewController: UIViewController {
+class DirectionsViewController: UIViewController, CLLocationManagerDelegate {
     
     public var event: Event?
     private let mapView = DirectionsView()
     
     let locationManager = CLLocationManager()
-    var currentLocation: CLLocation?
+    var currentLocation: CLLocationCoordinate2D?
     
     
     private var listener: ListenerRegistration!
@@ -26,18 +26,68 @@ class DirectionsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchEventLocation()
+        locationManager.delegate = self
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            //we need to say how accurate the data should be
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            //            myMapView.showsUserLocation = true
+            //            mapView.sh
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+            //            myMapView.showsUserLocation = true
+
 
     }
     
+    }
     
+//    var lat1 : NSString = self.venueLat
+//    var lng1 : NSString = self.venueLng
+//
+//    var latitude:CLLocationDegrees =  lat1.doubleValue
+//    var longitude:CLLocationDegrees =  lng1.doubleValue
+//
+//    var coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+//
+//    var placemark : MKPlacemark = MKPlacemark(coordinate: coordinate, addressDictionary:nil)
+//
+//    var mapItem:MKMapItem = MKMapItem(placemark: placemark)
+//
+//    mapItem.name = "Target location"
+//
+//    let launchOptions:NSDictionary = NSDictionary(object: MKLaunchOptionsDirectionsModeDriving, forKey: MKLaunchOptionsDirectionsModeKey)
+//
+//    var currentLocationMapItem:MKMapItem = MKMapItem.mapItemForCurrentLocation()
+//
+//    MKMapItem.openMapsWithItems([currentLocationMapItem, mapItem], launchOptions: launchOptions)
+    
+
     func fetchEventLocation() {
         guard let venueLat = self.event?.locationLat,
-            let venueLong = self.event?.locationLong else {return}
+        let venueLong = self.event?.locationLong else {return}
         let coordinate = CLLocationCoordinate2DMake(venueLat,venueLong)
-        let coordinate2 = CLLocationCoordinate2DMake(MKUserLocation, <#T##longitude: CLLocationDegrees##CLLocationDegrees#>)
-        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coordinate, addressDictionary:nil))
-        let mapItem2 = MKMapItem(placemark: MKPlacemark(coordinate: coordinate2))
-        mapItem.name = self.event?.eventName
+        
+        let placemark:MKPlacemark = MKPlacemark(coordinate: coordinate, addressDictionary: nil)
+        
+        let mapItem:MKMapItem = MKMapItem(placemark: placemark)
+        mapItem.name = "\(event?.eventName)"
+        
+        let launchOptions:NSDictionary = NSDictionary(object: MKLaunchOptionsDirectionsModeDriving, forKey: MKLaunchOptionsDirectionsModeKey as NSCopying)
+        
+        let currentLocationMapItem:MKMapItem = MKMapItem.forCurrentLocation()
+        
+        MKMapItem.openMaps(with: [currentLocationMapItem, mapItem], launchOptions: launchOptions as! [String : Any])
+        
+        
+//        let mapItem = MKMapItem(placemark: MKPlacemark(coordinate: coordinate, addressDictionary:nil))
+//        let mapItem2 = MKMapItem(placemark: MKPlacemark(coordinate: currentLocation!))
+//        let mapItem3 = MKMapItem()
+//        pl.name = self.event?.eventName
+        
+        
+        
 //        mapItem.openInMaps(launchOptions: [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving])
     }
     
